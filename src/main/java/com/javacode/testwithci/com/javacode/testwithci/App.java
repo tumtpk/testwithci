4ce33fee58291e50333e08bab9f23b65ee4ca592
tumtpk <pk.prateep@gmail.com>
2018-08-26 15:46:23 +0700

    init

---
+++ src/main/java/com/javacode/testwithci/com/javacode/testwithci/App.java
@@ -0,0 +1,13 @@
+package com.javacode.testwithci.com.javacode.testwithci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
